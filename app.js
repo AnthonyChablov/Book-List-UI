046e50ae946494a07c49308a8c121dfb86820781
Anthony Chablov <achablov@myseneca.ca>
2022-04-16 23:42:35 -0400

    Added local storage functionality

--- app.js
+++ app.js
@@ -44,28 +44,80 @@ class UI{ // UI constructor
         document.getElementById('author').value ='',
         document.getElementById('isbn').value ='';
     }
+    
 } 
 
+// Local storage class
+class Storage{
+    static getBooks(){
+        let books;
+        if (localStorage.getItem('books') === null){
+            books = [];
+        }
+        else{
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    static displayBooks(){  
+        const books= Storage.getBooks();
+        const ui = new UI;
+        books.forEach(book => {
+            // Add book to UI
+            ui.addBookToList(book);
+        });
+    }
+    static addBook(book){ // add item to local storage 
+        const books = Storage.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+    static removeBook(ISBN){
+        const books = Storage.getBooks();
+        books.forEach((book, i) => 
+            book.ISBN === ISBN ?
+                books.splice(i,1) : '');
+                
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+    
+}
+
 // Event Listeners
+// Dom Load Event
+document.addEventListener('DOMContentLoaded', Storage.displayBooks);
+
+
+
+
 // Add Book
 let bookForm = document.getElementById('book-form').addEventListener('submit', (e) => {
     let title = document.getElementById('title').value, // get form values 
         author = document.getElementById('author').value,
         ISBN = document.getElementById('isbn').value;
+
     const book = new Book(title, author, ISBN); // instantiate book
     const ui = new UI(); // instantiate ui
+
     title === '' || author === '' || ISBN == '' ? // Validation of entered books
         ui.showAlert('Please fill in all fields', 'error') : // show alert 
-        ui.addBookToList(book), ui.showAlert('Book Added', 'success'), ui.clear(); // show success
+        ui.addBookToList(book), // add book to list
+        ui.showAlert('Book Added', 'success'), // show success
+        ui.clear(), // clear entries
+        Storage.addBook(book); // add book to local storage
+    
     e.preventDefault(); // prevent default state
 });
+
 // Delete Book
 let bookList = document.getElementById('book-list').addEventListener
 ('click', (e)=>{
+    // instantiate ui
     const ui = new UI();
+    // delete book
     ui.deleteBook(e.target);
+    // remove from local storage
+    Storage.removeBook(e.target.parentElement.previousElementSibling.textContent);
     // show confirmation msg that book has been deleted
-    
-    
     e.preventDefault();
 },true);
